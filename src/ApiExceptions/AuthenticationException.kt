abae3fa1e0f35aad72b0a6319cbed43f129c2347
May Madany <may-mohamed-abo.madany.1@ens.etsmtl.ca>
2019-11-23 12:51:16 -0500

    adding checks for empty strings

--- src/ApiExceptions/AuthenticationException.kt
+++ src/ApiExceptions/AuthenticationException.kt
@@ -7,3 +7,5 @@ open class AuthenticationException(val status: HttpStatusCode, override val mess
 val UserNotFound = AuthenticationException(HttpStatusCode.NotFound, "The specified user could not be found")
 
 val UserAlreadyExists = AuthenticationException(HttpStatusCode.Conflict, "The specified user already exists")
+
+val BadUserFormat = AuthenticationException(HttpStatusCode.BadRequest, "User's fields can not have empty strings")
--- src/model/WhatsNew.kt
+++ src/model/WhatsNew.kt
@@ -1,9 +1,11 @@
 package ca.etsmtl.applets.notre_dame.model
 
+import ca.etsmtl.applets.notre_dame.ApiExceptions.BadUserFormat
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
 import org.litote.kmongo.Id
 import org.litote.kmongo.newId
+import kotlin.reflect.full.memberProperties
 
 @KtorExperimentalLocationsAPI
 @Location("/whatsNew/{version}")
@@ -13,10 +15,21 @@ data class WhatsNew(
     var description : String,
     val version : Float ){
 
+    init{
+        for (prop in WhatsNew::class.memberProperties){
+            var propVal = prop.get(this)
+            if ( propVal is String )
+            {
+                if (propVal.toString().isBlank())
+                    throw BadUserFormat
+            }
+        }
+    }
+
     fun patchWhatsNew(other: WhatsNewPatch)  {
-        if (other.title != null)
+        if (!other.title.isNullOrBlank())
             this.title =other.title!!
-        if (other.description  != null)
+        if (!other.description.isNullOrBlank())
             this.description =other.description!!
     }
 }
