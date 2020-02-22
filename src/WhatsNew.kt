f77cc9551e093966bebb2afd00f956ea6ec6e585
May Madany <may-mohamed-abo.madany.1@ens.etsmtl.ca>
2020-02-22 14:07:05 -0500

    cleaning app

--- src/WhatsNew.kt
+++ src/WhatsNew.kt
@@ -1,21 +1,17 @@
 package ca.etsmtl.applets.notre_dame
 
-import ca.etsmtl.applets.notre_dame.repository.UsersRepo
 import ca.etsmtl.applets.notre_dame.server.Server
 import org.kodein.di.Kodein
 import org.kodein.di.direct
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.eagerSingleton
 import org.kodein.di.generic.instance
-import org.kodein.di.generic.singleton
-import org.litote.kmongo.KMongo
+
 
 fun main(args: Array<String>) {
 
     val kodein = Kodein {
-        bind("mongoClient") from singleton { KMongo.createClient() }
         bind<Server>() with eagerSingleton { Server(kodein) }
-        bind(tag = "usersRepo") from singleton { UsersRepo(instance("mongoClient")) }
     }
     var app: Server = kodein.direct.instance()
     app.startServer()
